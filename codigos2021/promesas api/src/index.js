df9dd57fd290dd9d4976617d606b3e6f264e8408
Develop and System <37659043+DevelopSys@users.noreply.github.com>
2020-11-25 10:33:09 +0100

    update

--- codigos2021/promesas api/src/index.js
+++ codigos2021/promesas api/src/index.js
@@ -1,1 +1,40 @@
 import "./style.css";
+
+import { getAllUsers, getUserByUUID } from "./services/conexiones_http";
+
+window.mostrarDetalle = mostrarDetalle;
+
+$("#busqueda_boton").click(function (e) {
+  let numeroUsuarios = document.querySelector("#busqueda_texto").value;
+
+  if (numeroUsuarios) {
+    getAllUsers(numeroUsuarios).then((data) => {
+      data.results.forEach((element) => {
+        console.log(element);
+        representarUsuario(element, "#general");
+      });
+    });
+  } else {
+    alert("Por favor introduce un número");
+  }
+});
+
+function representarUsuario(usuario, sitio) {
+  //console.log(usuario);
+  $(`${sitio}`).append(
+    `<div class="card" style="width: 18rem;">
+  <img src="${usuario.picture.medium}" class="card-img-top" alt="${usuario.name.first} ${usuario.name.last}">
+  <div class="card-body">
+    <h5 class="card-title">${usuario.name.title} ${usuario.name.first} ${usuario.name.last}</h5>
+    <button value="${usuario.uuid}" class="btn btn-primary btn-block" onclick="mostrarDetalle(${usuario.uuid})">Ver detalles</button>
+  </div>
+  </div>`
+  );
+}
+
+function mostrarDetalle(uuid) {
+  getUserByUUID(uuid).then((data) => {
+    console.log(data);
+    representarUsuario(data.results[0], "#detalle");
+  });
+}
--- codigos2021/promesas api/src/services/conexiones_http.js
+++ codigos2021/promesas api/src/services/conexiones_http.js
@@ -1,6 +1,15 @@
 "use strict";
-let url =
-  "http://newsapi.org/v2/top-headlines?country=us&apiKey=3b9f157a5b75463189508b079bdbb25fwerwrwerwer";
-let urlUsers = "https://reqres.in/api/";
 
 let urlRandom = "https://randomuser.me/api/";
+
+let urlRandomEmail = "?email=nadir.carvalho@example.com";
+
+export async function getAllUsers(numero) {
+  let respuesta = await $.get(`${urlRandom}?results=${numero}`, "json");
+  return respuesta;
+}
+
+export async function getUserByUUID(uuid) {
+  let respuesta = await $.get(`${urlRandom}?email=${uuid}`, "json");
+  return respuesta;
+}
