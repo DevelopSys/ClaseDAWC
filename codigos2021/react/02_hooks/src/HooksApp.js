246c6ac8fefaaf10326b19447ce948b352ece7bd
Develop and System <37659043+DevelopSys@users.noreply.github.com>
2021-01-20 21:02:50 +0100

    update

--- codigos2021/react/02_hooks/src/HooksApp.js
+++ codigos2021/react/02_hooks/src/HooksApp.js
@@ -2,10 +2,19 @@ import React from "react";
 import HookUseState from "./components/HookUseState";
 
 const HooksApp = () => {
+  let numeroPasar = 8;
   return (
     <>
       <h1>Aplicación para explicar funcionamiento hooks</h1>
-      <HookUseState />
+      <input
+        className="form-control"
+        placeholder="Introduce valor inicial"
+        type="number"
+      ></input>
+      <button className="btn btn-primary" onClick={}>
+        Pasar valor
+      </button>
+      <HookUseState numero={numero} />
     </>
   );
 };
--- codigos2021/react/02_hooks/src/components/HookUseState.js
+++ codigos2021/react/02_hooks/src/components/HookUseState.js
@@ -1,10 +1,28 @@
 import React, { useState } from "react";
 
-const HookUseState = () => {
-  const [state, setState] = useState(1);
+const HookUseState = (props) => {
+  const [counter, setCounter] = useState(props.numero);
 
   // const state
   // const setState
+
+  let operacion;
+
+  const handleOnClick = ({ target }) => {
+    //console.log(e);
+    switch (target.name) {
+      case "incremento":
+        setCounter(counter + 1);
+        break;
+      case "decremento":
+        setCounter(counter - 1);
+        break;
+      case "reset":
+        setCounter(0);
+        break;
+    }
+    //setCounter(counter + 1);
+  };
 
   return (
     <>
@@ -12,16 +30,44 @@ const HookUseState = () => {
       <div className="row">
         <div className="col">
           <h3>Ejemplo inicial</h3>
-          <p>El valor del contador es {state}</p>
+          <p>El valor del contador es {counter}</p>
           <button
-            onClick={() => {
-              //setState(state + 1);
-              setState((w) => {
-                return w + 1;
-              });
-            }}
+            name="incremento"
+            className="btn btn-primary"
+            onClick={
+              handleOnClick
+
+              /*() => {
+              //counter++;
+              //setCounter(counter + 1);
+
+            }*/
+            }
           >
-            Pulsar
+            +1
+          </button>
+          <button
+            name="decremento"
+            className="btn btn-primary"
+            onClick={
+              handleOnClick /*() => {
+              //setCounter(counter - 1);
+              setCounter((estadoActual) => estadoActual - 1);
+            }*/
+            }
+          >
+            -1
+          </button>
+          <button
+            name="reset"
+            className="btn btn-primary"
+            onClick={
+              handleOnClick /*() => {
+              setCounter(5);
+            }*/
+            }
+          >
+            Reset
           </button>
         </div>
       </div>
