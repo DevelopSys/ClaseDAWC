1320769691bd690b099fedb250399bd58d10d7c4
Develop and System <developandsystem@gmail.com>
2021-11-26 19:46:34 +0100

    update

--- 01_JavaScript/codigos2122/ejercicio_promesas/utils/js/ligas.js
+++ 01_JavaScript/codigos2122/ejercicio_promesas/utils/js/ligas.js
@@ -1,14 +1,19 @@
 let selectLigas;
+let selectEquipos;
+let ligaSeleccionada
 
 window.addEventListener("load", () => {
   selectLigas = document.querySelector("#select-ligas");
+  selectEquipos = document.querySelector("#select-equipos");
   cargarLigas();
   selectLigas.addEventListener("change", (e) => {
-    let id = e.target.value;
-    cargarEquipos(id)
+    ligaSeleccionada = e.target.value;
+    cargarEquipos(ligaSeleccionada);
     // fetch cargar los equipos
     // quiero llegar al select
   });
+
+  selectEquipos.addEventListener("change", (e) => {cargarDetalle(e.target.value, ligaSeleccionada)});
 });
 
 function cargarLigas() {
@@ -20,7 +25,7 @@ function cargarLigas() {
     )
     .then(
       (res) => {
-          console.log(res.leagues);
+        console.log(res.leagues);
         res.leagues.forEach((element) => {
           let nodoOption = document.createElement("option");
           nodoOption.value = element.idLeague;
@@ -31,10 +36,52 @@ function cargarLigas() {
       (rej) => {}
     );
 }
-
 function cargarEquipos(id) {
-    let url = `https://www.thesportsdb.com/api/v1/json/2/lookup_all_teams.php?id=${id}`
-    console.log(url);
+  let url = `https://www.thesportsdb.com/api/v1/json/2/lookup_all_teams.php?id=${id}`;
+  let nodosBorrar = document.querySelectorAll("#select-equipos option");
+  nodosBorrar.forEach((e) => e.remove());
+  //document.querySelector("#select-equipos").childNodes.forEach((e) => {
+  //console.log(e);
+  //selectEquipos.removeChild(e);
+  //});
+
+  fetch(url)
+    .then(
+      (res) => res.json(),
+      (rej) => {}
+    )
+    .then(
+      (res) => {
+        res.teams.forEach((element) => {
+          let nodoOption = document.createElement("option");
+          nodoOption.value = element.idTeam;
+          nodoOption.innerHTML = `${element.strTeam}`;
+          selectEquipos.append(nodoOption);
+        });
+      },
+      (rej) => {}
+    );
+}
+
+function cargarDetalle(id, idLiga) {
+    let url = `https://www.thesportsdb.com/api/v1/json/2/lookup_all_teams.php?id=${idLiga}`;
+
+
+  fetch(url)
+    .then(
+      (res) => res.json(),
+      (rej) => {}
+    )
+    .then(
+      (res) => {
+        res.teams.forEach((element) => {
+          if (element.idTeam == id){
+              
+          }
+        });
+      },
+      (rej) => {}
+    );
 }
 
 // fetch(url) --> data.json --> json
