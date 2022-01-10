3dbbfe9b3032b11f3a9ed1cbf73da36fb7a3abdb
Develop and System <developandsystem@gmail.com>
2022-01-10 18:57:23 +0100

    update

--- 01_JavaScript/codigos2122/2o trimestre/ejercicio_express/primer_api/routes/users.js
+++ 01_JavaScript/codigos2122/2o trimestre/ejercicio_express/primer_api/routes/users.js
@@ -1,22 +1,26 @@
 var express = require("express");
+const { response } = require("../app");
 var router = express.Router();
 
+let usuariosFiltro = [];
+
 let usuarios = [
-  { name: "Borja", age: 20, sex: "male"},
-  { name: "Claudia", age: 20, sex: "female"},
-  { name: "Pepe", age: 20,sex: "male"},
-  { name: "Luisa", age: 20, sex: "female"},
-  { name: "Jose", age: 20, sex: "male"},
-  { name: "Luis", age: 20, sex: "male"},
-  { name: "Luisa", age: 20, sex: "female"},
-  { name: "Celia", age: 20, sex: "female"},
-  { name: "Claudia", age: 20, sex: "female"},
-  { name: "Maria", age: 20, sex: "female"},
-  { name: "Luis", age: 20, sex: "male"},
-  { name: "Marta", age: 20, sex: "female"}]
+  { name: "Borja", age: 20, sex: "male" },
+  { name: "Claudia", age: 20, sex: "female" },
+  { name: "Pepe", age: 20, sex: "male" },
+  { name: "Luisa", age: 20, sex: "female" },
+  { name: "Jose", age: 20, sex: "male" },
+  { name: "Luis", age: 20, sex: "male" },
+  { name: "Luisa", age: 20, sex: "female" },
+  { name: "Celia", age: 20, sex: "female" },
+  { name: "Claudia", age: 20, sex: "female" },
+  { name: "Maria", age: 20, sex: "female" },
+  { name: "Luis", age: 20, sex: "male" },
+  { name: "Marta", age: 20, sex: "female" },
+];
 
 /* GET users listing. */
-router.get("/",  (req, res, next) => {
+router.get("/", (req, res, next) => {
   res.send(`<p>API Endpoints<p>
   <ul>
     <li><a href="./users/all">Todos</a></li>
@@ -37,8 +41,23 @@ router.get("/all", function (req, res, next) {
   });
 });
 
+router.get("/male", (req, res, next) => {
+  usuariosFiltro = usuarios.filter((item) => item.sex == "male");
+  res.json({ response: [usuariosFiltro] });
+});
+
+router.get("/female", (req, res, next) => {
+  usuariosFiltro = usuarios.filter((item) => item.sex == "female");
+
+  res.json({ response: [usuariosFiltro] });
+});
+
+router.get("/filter/:sex", (req, res, next) => {
+  usuariosFiltro = usuarios.filter((item) => item.sex == "female");
+
+  res.json({ response: [usuariosFiltro] });
+});
+
 // plantear la peticion de male y female para que solo me den los usuarios del sex concreto
-
-
 
 module.exports = router;
