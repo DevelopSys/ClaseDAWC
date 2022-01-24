b94723453ee6b67b60dc987c7f8bed82cfe24ad7
Develop and System <developandsystem@gmail.com>
2022-01-24 18:27:22 +0100

    Update usuarios.js

--- 01_JavaScript/codigos2122/2o trimestre/ejercicio_apimysql/apimyql/routes/usuarios.js
+++ 01_JavaScript/codigos2122/2o trimestre/ejercicio_apimysql/apimyql/routes/usuarios.js
@@ -13,13 +13,71 @@ router.get("/selectAll", function (req, res, next) {
         .send({ code: 1, message: "Error en la conexion", error: err });
     } else {
       // query
-      res
+      conexionActual.query(
+        "SELECT * FROM usuarios WHERE genero = (?)",
+        ["Masculino"],
+        (err, data) => {
+          if (err) {
+            res.status(300).send({
+              code: 2,
+              message: "Query ejecutada con error",
+              error: err,
+            });
+          } else {
+            res.status(200).send({
+              code: 2,
+              message: "datos correctos",
+              resutls: data,
+            });
+          }
+        }
+      );
+      /* res
         .status(200)
         .send({ code: 2, message: "Conexion correcta con la base de datos" });
 
-      conexionActual.end();
+      conexionActual.end(); */
     }
   });
 });
+
+/**
+ * 
+ * conexionActual.query(
+        "SELECT * FROM usuarios WHERE genero = (?)",
+        ["Masculino"],
+        (err, data) => {
+          if (err) {
+            res.status(300).send({
+              code: 2,
+              message: "Query ejecutada con error",
+              error: err,
+            });
+          } else {
+            res.status(200).send({
+              code: 2,
+              message: "datos correctos",
+              resutls: data,
+            });
+          }
+        }
+ */
+// obtener el elemento cuyo id es el indicado en la ruta
+// localhost: 3000 / usuarios / id / 8; -->
+
+/*
+{
+  "code": 2,
+  "message": "datos correctos",
+  "results": [
+    {
+      "id": 8,
+      "nombre": "Borja",
+      "genero": "Masculino"
+    }
+  ], 
+  number: 1
+}
+*/
 
 module.exports = router;
