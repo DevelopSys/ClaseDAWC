f906890fc0615843b5d4a3149896174351089585
Develop and System <developandsystem@gmail.com>
2022-02-07 15:59:25 +0100

    Update ComponenteState.js

--- 02_React/codigos2122/proyecto_hooks/src/components/ComponenteState.js
+++ 02_React/codigos2122/proyecto_hooks/src/components/ComponenteState.js
@@ -1,7 +1,45 @@
-import React from 'react';
-import './ComponenteState.css';
+import React, { useState } from "react";
+import "./ComponenteState.css";
+
 function ComponenteState() {
-  return <div><h1>Componente State</h1></div>;
+  //let elementos = ["Opcion 1", "Opcion 2", "Opcion 3", "Opcion 4"];
+
+  const [elementos, setElementos] = useState([]);
+  // elementos = []
+
+  return (
+    <div>
+      <h1>Componente State</h1>
+      <div className="m-4">
+        <input
+          className="form-control"
+          placeholder="Introduce elemento agregar"
+        ></input>
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            setElementos((e) => {
+              return [...e, "Nuevo"];
+            });
+          }}
+        >
+          Agregar
+        </button>
+        <button
+          onClick={() => {
+            console.log(elementos);
+          }}
+        >
+          Comprobar
+        </button>
+      </div>
+      <ul>
+        {elementos.map((d) => (
+          <li>{d}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ComponenteState;
