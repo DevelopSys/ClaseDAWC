f2dd5d57297759e0afc839a9b9e7df8ef998dc3e
Develop and System <developandsystem@gmail.com>
2022-02-07 18:57:03 +0100

    Update ComponenteState.js

--- 02_React/codigos2122/proyecto_hooks/src/components/ComponenteState.js
+++ 02_React/codigos2122/proyecto_hooks/src/components/ComponenteState.js
@@ -5,22 +5,35 @@ function ComponenteState() {
   //let elementos = ["Opcion 1", "Opcion 2", "Opcion 3", "Opcion 4"];
 
   const [elementos, setElementos] = useState([]);
+  const [elementoInput, setElementoInput] = useState("");
+  const [elementoEmail, setElementoEmail] = useState("");
+  const [elementoPass, setElementoPass] = useState("");
+  const [elementoExperiencia, setElementoExperiencia] = useState(false);
+  const [formularios, setFormularios] = useState([]);
+
   // elementos = []
+
+  let datoInput = "";
 
   return (
     <div>
       <h1>Componente State</h1>
-      <div className="m-4">
+      <div classNameName="m-4">
         <input
-          className="form-control"
+          classNameName="form-control"
           placeholder="Introduce elemento agregar"
+          onChange={(e) => {
+            setElementoInput(e.target.value);
+          }}
+          value={elementoInput}
         ></input>
         <button
-          className="btn btn-primary"
+          classNameName="btn btn-primary"
           onClick={() => {
             setElementos((e) => {
-              return [...e, "Nuevo"];
+              return [...e, elementoInput];
             });
+            setElementoInput("");
           }}
         >
           Agregar
@@ -38,6 +51,68 @@ function ComponenteState() {
           <li>{d}</li>
         ))}
       </ul>
+
+      {/* 
+        AL DAR AL BOTON SUBMIT
+        [{email: emailInput, password: passwordInput, experiencia: experienciaInput}]
+*/}
+      <div className="mb-3">
+        <label for="exampleInputEmail1" className="form-label">
+          Email address
+        </label>
+        <input
+          onChange={(e) => {
+            setElementoEmail(e.target.value);
+          }}
+          value={elementoEmail}
+          type="email"
+          className="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        ></input>
+      </div>
+      <div className="mb-3">
+        <label for="exampleInputPassword1" className="form-label">
+          Password
+        </label>
+        <input
+          onChange={(e) => {
+            setElementoPass(e.target.value);
+          }}
+          value={elementoPass}
+          type="password"
+          className="form-control"
+          id="exampleInputPassword1"
+        ></input>
+      </div>
+      <div className="mb-3 form-check">
+        <input
+          onChange={(e) => {
+            setElementoExperiencia(e.target.value);
+          }}
+          type="checkbox"
+          className="form-check-input"
+          id="exampleCheck1"
+        ></input>
+        <label className="form-check-label" for="exampleCheck1">
+          Check me out
+        </label>
+      </div>
+      <button
+        className="btn btn-primary"
+        onClick={() => {
+          setFormularios((e) => [
+            ...e,
+            {
+              email: { elementoEmail },
+              pass: { elementoPass },
+              expeciencia: { elementoExperiencia },
+            },
+          ]);
+        }}
+      >
+        Submit
+      </button>
     </div>
   );
 }
