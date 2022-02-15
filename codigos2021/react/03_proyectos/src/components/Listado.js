f5b16bd73363c82d43e227e861979b65f11ba90e
Develop and System <developandsystem@gmail.com>
2022-02-15 20:28:38 +0100

    update

--- codigos2021/react/03_proyectos/src/components/Listado.js
+++ codigos2021/react/03_proyectos/src/components/Listado.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Listado = (props) => {
+  useEffect(() => {
+    // cuando algo del componente es recargado
+
+    fetch("")
+      .then((d) => {
+        return d.json();
+      })
+      .then((d) => {
+        setConsulta(d.results);
+      });
+    // cuando el componente es despilado
+    return () => {};
+  }, []);
+
+  const [consulta, setConsulta] = useState([]);
+
   return (
     <>
       <h2>Detalles proyectos</h2>
+
+      {consulta.map(e=>{})}
       <div className="row">
         {props.proyects.map((data) => (
           <div class="card" style={{ width: "18rem;" }}>
