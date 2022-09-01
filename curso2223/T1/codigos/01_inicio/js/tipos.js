f10ecf23eacf31f4407bfb6171fc357eeac1f692
Develop and System <developandsystem@gmail.com>
2022-09-01 16:56:24 +0200

    update

--- curso2223/T1/codigos/01_inicio/js/tipos.js
+++ curso2223/T1/codigos/01_inicio/js/tipos.js
@@ -2,3 +2,26 @@ let numeroEntero = 30;
 let numeroDecimales = 3.14;
 console.log(`El valor de numeroEntero es ${numeroEntero}`);
 console.log(`El valor de numeroDecimales es ${numeroDecimales}`);
+
+let nombre = "Borja";
+let profesion = "Profesor";
+
+console.log(`Mi nombre es ${nombre}`);
+console.log("Mi prpofesion es la de " + profesion);
+
+console.log(`La suma de 6 y 7 es ${6 + 7}`);
+
+console.log("Ejemplo de impresión\nCon salto de línea");
+
+let decision = false;
+console.log(`El resultado de la decisión es ${false}`);
+decision && console.log("Es una decision correcta ");
+
+decision
+  ? console.log("La decisión ha sido correcta")
+  : console.log("La decisión ha sido falsa");
+
+let sinValor = null;
+let sinDefinir;
+console.log(`El valor de la primera variable es ${sinValor}`);
+console.log(`El valor de la segunda variable es ${sinDefinir}`);
