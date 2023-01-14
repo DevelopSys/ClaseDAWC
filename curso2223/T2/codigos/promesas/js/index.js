12e7bc39087524d27ff139a9efffdcd70c91dcb0
Develop and System <developandsystem@gmail.com>
2023-01-14 09:10:34 +0100

    update

--- curso2223/T2/codigos/promesas/js/index.js
+++ curso2223/T2/codigos/promesas/js/index.js
@@ -29,14 +29,70 @@ evaluarLenguaje(lenguajes[1], (resultado) => {
 
 async function promesaPropia() {
   let numero = parseInt(Math.random() * 100);
-  if (numero > 50) {
-    return Promise.resolve(numero);
-  } else {
-    return Promise.reject("Pocas posibilidades");
-  }
+
+  return new Promise((res, rej) => {
+    if (numero >= 50) {
+      res(numero);
+    } else {
+      rej("Pocas posibilidades");
+    }
+  });
 }
 
-console.log(promesaPropia());
+async function obtenerResultado() {
+  let resultado = await promesaPropia();
+  console.log(resultado);
+}
+
+//"https://cors-anywhere.herokuapp.com/https://github.com/annexare/Countries/blob/master/data/countries.json"
+/* 
+
+fetch("https://dummyjson.com/products")
+  .then((res) => {
+    return res.json();
+  })
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((rej) => {
+    console.log(`Ha habido algún tipo de fallo: ${rej}`);
+  }); */
+
+async function obtenerProductos() {
+  let resultado = await fetch("https://dummyjson.com/products");
+  let resultadosJSON = resultado.json();
+  return resultadosJSON;
+}
+
+/* console.time("promesa normal");
+fetch("https://dummyjson.com/products")
+  .then((res) => {
+    return res.json();
+  })
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((rej) => {
+    console.log(`Ha habido algún tipo de fallo: ${rej}`);
+  });
+console.timeEnd("promesa normal"); */
+
+console.time("promesa await");
+let fila = document.querySelector("#productos");
+obtenerProductos().then((res) => {
+  res.products.forEach((element) => {
+    fila.innerHTML += `<div class="col">
+    <div class="card m-4" style="width: 18rem;">
+    <img src="${element.images[0]}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${element.title}</h5>
+      <p class="card-text">${element.description}</p>
+    </div>
+  </div>
+  </div>`;
+  });
+});
+console.timeEnd("promesa await");
 
 /* promesaPropia()
   .then((ok) => {
