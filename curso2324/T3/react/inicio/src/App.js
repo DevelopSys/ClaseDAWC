900c7bcdf14e39e55cc1a386122fbf9aefd6e8e8
Develop and System <developandsystem@gmail.com>
2024-01-22 16:49:50 +0100

    update

---
+++ curso2324/T3/react/inicio/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Presentacion from "./components/Presentacion";
+
+function App() {
+  return (
+    <div>
+      <h1>Esta es la primera app con react </h1>
+      <p>Esta aplicacion nos servirá para entender como funciona react</p>
+      <Presentacion />
+    </div>
+  );
+}
+
+export default App;
---
+++ curso2324/T3/react/inicio/src/components/Presentacion.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Presentacion = () => {
+  let nombre = "Borja";
+  return (
+    <div>
+      <h2>Presentacion</h2>
+      <p>Esto es un componente funcional y esta ralizado por {nombre}</p>
+    </div>
+  );
+};
+
+export default Presentacion;
